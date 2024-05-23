17dbfb39cf06a0bd39e3fc4ed997c9b9a609007e
MOHD AMIR AHMAD <amir.engineer786@gmail.com>
2024-05-24 00:09:20 +0200

    Enhance Serialization to serialize Multipleusers

--- src/Serialization/DeserializeDemo.java
+++ src/Serialization/DeserializeDemo.java
@@ -3,13 +3,22 @@ package Serialization;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DeserializeDemo {
     public static void main(String[] args) {
-        User user = null;
 
-        //try-with-resource
-        try (FileInputStream fileIn = new FileInputStream("user.ser");
+        DeserializeDemo demo = new DeserializeDemo();
+        demo.deSerializingSingleUser();
+        demo.deSerializingMultipleUser();
+    }
+
+
+    public void deSerializingSingleUser() {
+
+        User user = null;
+        try (FileInputStream fileIn = new FileInputStream("singleUser.ser");
              ObjectInputStream in = new ObjectInputStream(fileIn))
         {
             user = (User) in.readObject();
@@ -22,5 +31,29 @@ public class DeserializeDemo {
 
         System.out.println("Deserialized User...");
         System.out.println(user);
+
+
     }
+
+    public void deSerializingMultipleUser() {
+        List<User> userList = new ArrayList<>();
+
+        //try-with-resource
+        try (FileInputStream fileIn = new FileInputStream("userList.ser");
+             ObjectInputStream in = new ObjectInputStream(fileIn))
+        {
+            userList = (List<User>) in.readObject();
+        } catch (IOException i) {
+            i.printStackTrace();
+        } catch (ClassNotFoundException c) {
+            System.out.println("User class not found");
+            c.printStackTrace();
+        }
+
+        System.out.println("Deserialized User List...");
+        System.out.println(userList);
+
+    }
+
+
 }
--- src/Serialization/SerializeDemo.java
+++ src/Serialization/SerializeDemo.java
@@ -3,17 +3,44 @@ package Serialization;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class SerializeDemo {
     public static void main(String[] args) {
-        User user = new User(1, "Mohd Amir Ahmad", "password123");
 
-        //try-with-resource
-        try (FileOutputStream fileOut = new FileOutputStream("user.ser");
+        SerializeDemo demo = new SerializeDemo();
+        demo.serializeSingleUser();
+        demo.serializeMultipleUsers();
+
+    }
+
+    public void serializeSingleUser() {
+        User user = new User(1, "Yashasvi Jaiswal", "password1234");
+
+        try (FileOutputStream fileOut = new FileOutputStream("singleUser.ser");
              ObjectOutputStream out = new ObjectOutputStream(fileOut))
         {
             out.writeObject(user);
-            System.out.println("Serialized data is saved in user.ser");
+            System.out.println("Serialized single user data is saved in singleUser.ser");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+
+    public void serializeMultipleUsers() {
+        User user1 = new User(1, "Rohit Sharma", "password1");
+        User user2 = new User(2, "Virat Kohli", "password2");
+        User user3 = new User(3, "MS Dhoni", "password3");
+        User user4 = new User(4, "Rinku Singh", "password4");
+
+        List<User> userList = Arrays.asList(user1, user2, user3, user4);
+
+        try (FileOutputStream fileOut = new FileOutputStream("userList.ser");
+             ObjectOutputStream out = new ObjectOutputStream(fileOut))
+        {
+            out.writeObject(userList);
+            System.out.println("Serialized multiple users data is saved in userList.ser");
         } catch (IOException i) {
             i.printStackTrace();
         }
